209154c808f679f39282aefbdefabe06bf7fff7c
NoeliaGaray <103612855+NoeliaGaray@users.noreply.github.com>
2022-05-22 19:14:13 -0300

    TERMINADO

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,38 @@
-let vector1 : number[] = new Array (6);
-let nro : number, indice : number;
+let vector1: number[] = new Array(6);
+let nro: number, indice: number;
 for (indice = 0; indice < 6; indice++) {
-nro = Number(prompt(`Indique el número que desea incorporar en la posición ${indice} del primer vector: `));
-vector1[indice] = nro;
+  nro = Number(
+    prompt(
+      `Ingrese el número que desea incorporar en la posición ${indice} del primer vector: `
+    )
+  );
+  vector1[indice] = nro;
 }
 for (indice = 0; indice < 6; indice++) {
-console.log(`El número en la posición ${indice} es ${vector1[indice]}`);
+  console.log(
+    `El número en la posición ${indice} del primer vector es ${vector1[indice]}`
+  );
 }
-let vector2 : number[] = new Array (6);
+let vector2: number[] = new Array(6);
 for (indice = 0; indice < 6; indice++) {
-nro = Number(prompt(`Indique el número que desea incorporar en la posición ${indice} del segundo vector: `));
-vector2[indice] = nro;
+  nro = Number(
+    prompt(
+      `Ingrese el número que desea incorporar en la posición ${indice} del segundo vector: `
+    )
+  );
+  vector2[indice] = nro;
 }
 for (indice = 0; indice < 6; indice++) {
-console.log(`El número en la posición ${indice} es ${vector2[indice]}`);
+  console.log(
+    `El número en la posición ${indice} del segundo vector es ${vector2[indice]}`
+  );
 }
-let sumaDeVectores : number[] = new Array (6);
+let sumaDeVectores: number[] = new Array(6);
 for (indice = 0; indice < 6; indice++) {
-indice=vector1[indice]+vector2[indice]
+  sumaDeVectores[indice] = vector1[indice] + vector2[indice];
 }
 for (indice = 0; indice < 6; indice++) {
-console.log(`La suma de los vectores es ${sumaDeVectores[vector1[indice+vector2[indice]]]}`);
+  console.log(
+    `El resultado es ${vector1[indice]}+${vector2[indice]}=${sumaDeVectores[indice]}`
+  );
 }
